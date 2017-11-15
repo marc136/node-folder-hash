8a460e5a4a57167c115d5c31b9b5ac24649b1308
samuelthomps0n <sam@samuelthompson.co.uk>
2017-11-15 11:04:22 +0000

    Change example filenames
    The example filenames are currently the wrong way round, with the callbacks
    having promises and vice versa.

--- examples/readme-with-promises.js
+++ examples/readme-with-promises.js
@@ -4,22 +4,24 @@
 var hasher = require('../index.js');
 
 // pass element name and folder path separately
-hasher.hashElement('node_modules', __dirname, function (error, hash) {
-    if (error) return console.error('hashing failed:', error);
+hasher.hashElement('node_modules', __dirname).then(function (hash) {
     console.log('Result for folder "node_modules" in directory "' + __dirname + '":');
     console.log(hash.toString());
 });
 
 // pass element path directly
-hasher.hashElement(__dirname, function (error, hash) {
-    if (error) return console.error('hashing failed:', error);
+hasher.hashElement(__dirname).then(function (hash) {
     console.log('Result for folder "' + __dirname + '":');
     console.log(hash.toString());
 });
 
 // pass options (example: exclude dotFiles)
-var options = { excludes: ['**/.*'], match: { basename: false, path: true } };
-hasher.hashElement(__dirname, options, function (error, hash) {
-    console.log('Result for folder "' + __dirname + '":');
-    console.log(hash.toString());
+var options = { excludes: ['.*'], match: { basename: true, path: false } };
+hasher.hashElement(__dirname, options)
+.then(function (hash) {
+  console.log('Result for folder "' + __dirname + '":');
+  console.log(hash.toString());
+})
+.catch(function (error) {
+  return console.error('hashing failed:', error);
 });
