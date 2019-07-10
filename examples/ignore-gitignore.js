706ad48ff0da9e587de08b97c5b501eb6a1be5b9
bwain <bwain@grubhub.com>
2019-07-10 12:54:23 -0500

    Allow setting include and exclude as functions in addition to arrays

---
+++ examples/ignore-gitignore.js
@@ -0,0 +1,27 @@
+/**
+ * Shows how the exlcude option can be used with a function.
+ * 
+ * Real-life usage could be to exclude gitignored files
+ */
+
+const { hashElement } = require('../index')
+const fs = require('fs');
+const ignore = require('ignore'); 
+
+const gitignoreContents = fs.readFileSync('../.gitignore').toString().split('\n');
+const ig = ignore().add(gitignoreContents);
+
+function shouldExclude(name){
+  return ig.ignores(name);
+}
+
+hashElement('../', {
+  files: {
+    exclude: shouldExclude
+  },
+  folders: {
+    exclude: shouldExclude
+  }
+}).then(hash => {
+  console.log('hash of everything that is not gitignored: ', hash)
+});
